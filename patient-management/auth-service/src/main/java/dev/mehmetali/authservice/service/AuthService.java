4f890d504888c66bde15b6ce71c0852831a62623
kabamehmetali <mehmetalikabaa@icloud.com>
2026-03-24 19:09:48 -0400

    api-gateway completed

--- patient-management/auth-service/src/main/java/dev/mehmetali/authservice/service/AuthService.java
+++ patient-management/auth-service/src/main/java/dev/mehmetali/authservice/service/AuthService.java
@@ -5,6 +5,7 @@ import dev.mehmetali.authservice.dto.LoginResponseDto;
 import dev.mehmetali.authservice.model.User;
 import dev.mehmetali.authservice.repository.UserRepository;
 import dev.mehmetali.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -32,5 +33,14 @@ public class AuthService {
                 .map(u-> jwtUtil.generateToken(u.getEmail(), u.getRole()));
 
         return token;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
     }
 }
